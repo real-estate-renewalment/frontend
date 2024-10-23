87b4897179888a95127e94306e407372ffb9f7f6
LAPTOP-B29GO558\adamf <adamfinkler6@gmail.com>
2024-10-24 00:03:53 +0300

    changed the headers of the sidebar from left to right

--- src/layouts/full/sidebar/NavGroup/NavGroup.js
+++ src/layouts/full/sidebar/NavGroup/NavGroup.js
@@ -1,22 +1,21 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 // mui imports
-import { ListSubheader, styled } from '@mui/material';
+import { ListSubheader, styled } from "@mui/material";
 
 const NavGroup = ({ item }) => {
-  const ListSubheaderStyle = styled((props) => <ListSubheader disableSticky {...props} />)(
-    ({ theme }) => ({
-      ...theme.typography.overline,
-      fontWeight: '700',
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(0),
-      color: theme.palette.text.primary,
-      lineHeight: '26px',
-      padding: '3px 12px',
-    }),
-  );
-  return (
-    <ListSubheaderStyle>{item.subheader}</ListSubheaderStyle>
-  );
+  const ListSubheaderStyle = styled((props) => (
+    <ListSubheader disableSticky {...props} />
+  ))(({ theme }) => ({
+    ...theme.typography.overline,
+    fontWeight: "700",
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(0),
+    color: theme.palette.text.primary,
+    lineHeight: "26px",
+    padding: "3px 12px",
+    textAlign: "right",
+  }));
+  return <ListSubheaderStyle>{item.subheader}</ListSubheaderStyle>;
 };
 
 NavGroup.propTypes = {
