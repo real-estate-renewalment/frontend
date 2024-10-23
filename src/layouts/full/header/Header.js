e25e3cfd33d159fdc6065152ed70db2da4d14f8b
LAPTOP-B29GO558\adamf <adamfinkler6@gmail.com>
2024-10-23 23:46:09 +0300

    organized the buttons to be on the right, and the notifications and profile on the left

--- src/layouts/full/header/Header.js
+++ src/layouts/full/header/Header.js
@@ -1,71 +1,91 @@
-import React from 'react';
-import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button } from '@mui/material';
-import PropTypes from 'prop-types';
+import React from "react";
+
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  styled,
+  Stack,
+  IconButton,
+  Badge,
+  Button,
+} from "@mui/material";
+import PropTypes from "prop-types";
 
 // components
-import Profile from './Profile';
-import { IconBellRinging, IconMenu } from '@tabler/icons-react';
+import Profile from "./Profile";
+import { IconBellRinging, IconMenu } from "@tabler/icons-react";
 
 const Header = (props) => {
-
   // const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
   // const lgDown = useMediaQuery((theme) => theme.breakpoints.down('lg'));
 
-
   const AppBarStyled = styled(AppBar)(({ theme }) => ({
-    boxShadow: 'none',
+    boxShadow: "none",
     background: theme.palette.background.paper,
-    justifyContent: 'center',
-    backdropFilter: 'blur(4px)',
-    [theme.breakpoints.up('lg')]: {
-      minHeight: '70px',
+    justifyContent: "center",
+    backdropFilter: "blur(4px)",
+    [theme.breakpoints.up("lg")]: {
+      minHeight: "70px",
     },
   }));
   const ToolbarStyled = styled(Toolbar)(({ theme }) => ({
-    width: '100%',
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
     color: theme.palette.text.secondary,
   }));
 
   return (
     <AppBarStyled position="sticky" color="default">
       <ToolbarStyled>
-        <IconButton
-          color="inherit"
-          aria-label="menu"
-          onClick={props.toggleMobileSidebar}
-          sx={{
-            display: {
-              lg: "none",
-              xs: "inline",
-            },
-          }}
-        >
-          <IconMenu width="20" height="20" />
-        </IconButton>
+        <Box className="right-items-container" sx ={{display:'flex'}}>
+          <IconButton
+            color="inherit"
+            aria-label="menu"
+            onClick={props.toggleMobileSidebar}
+            sx={{
+              display: {
+                lg: "none",
+                xs: "inline",
+              },
+            }}
+          >
+            <IconMenu width="20" height="20" />
+          </IconButton>
 
+          <IconButton
+            size="large"
+            aria-label="show 11 new notifications"
+            color="inherit"
+            aria-controls="msgs-menu"
+            aria-haspopup="true"
+            sx={{
+              ...(typeof anchorEl2 === "object" && {
+                color: "primary.main",
+              }),
+            }}
+          >
+            <Badge variant="dot" color="primary">
+              <IconBellRinging size="21" stroke="1.5" />
+            </Badge>
+          </IconButton>
+          <Stack spacing={1} direction="row" alignItems="center">
+            <Profile />
+          </Stack>
+        </Box>
 
-        <IconButton
-          size="large"
-          aria-label="show 11 new notifications"
-          color="inherit"
-          aria-controls="msgs-menu"
-          aria-haspopup="true"
-          sx={{
-            ...(typeof anchorEl2 === 'object' && {
-              color: 'primary.main',
-            }),
-          }}
-        >
-          <Badge variant="dot" color="primary">
-            <IconBellRinging size="21" stroke="1.5" />
-          </Badge>
-
-        </IconButton>
-        <Box flexGrow={1} />
-        <Stack spacing={1} direction="row" alignItems="center">
-         
-          <Profile />
-        </Stack>
+        <Box className="left-items-container">
+          <Button onClick={console.log("button1")} sx={{ color: "#5A6A85" }}>
+            button1
+          </Button>
+          <Button onClick={console.log("button2")} sx={{ color: "#5A6A85" }}>
+            button2
+          </Button>
+          <Button onClick={console.log("button3")} sx={{ color: "#5A6A85" }}>
+            button3
+          </Button>
+        </Box>
       </ToolbarStyled>
     </AppBarStyled>
   );
